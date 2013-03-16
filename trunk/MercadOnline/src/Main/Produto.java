dd0aa172ac1e62ffecd594b55c45fb8d854f6e8c
cc_bf@hotmail.com <cc_bf@hotmail.com@04f99739-03aa-64aa-60d3-ec4ede2dcfab>
2013-03-16 00:45:52 +0000

    Criacao da classe Produto

--- trunk/MercadOnline/src/Main/Produto.java
+++ trunk/MercadOnline/src/Main/Produto.java
@@ -1,5 +1,73 @@
 package Main;
 
-public class Produto {
+import java.util.Date;
 
+public class Produto {
+	private int idProduto;
+	private String nome;
+	private String categoria;
+	private boolean promocao;
+	private boolean sugerido;
+	private Date validade;
+	private String marca;
+	private float peso;
+	private double preco;
+	public int getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public boolean isPromocao() {
+		return promocao;
+	}
+	public void setPromocao(boolean promocao) {
+		this.promocao = promocao;
+	}
+	public boolean isSugerido() {
+		return sugerido;
+	}
+	public void setSugerido(boolean sugerido) {
+		this.sugerido = sugerido;
+	}
+	public Date getValidade() {
+		return validade;
+	}
+	public void setValidade(Date validade) {
+		this.validade = validade;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+	
 }
