7cca30070cfd354a65e550e3274ac697f8358498
cc_bf@hotmail.com <cc_bf@hotmail.com@04f99739-03aa-64aa-60d3-ec4ede2dcfab>
2013-03-16 00:52:43 +0000

    Criacao das classes Gerente e Caixa

---
+++ MercadOnline/src/Main/Caixa.java
@@ -0,0 +1,32 @@
+package Main;
+
+public class Caixa extends Usuario{
+
+	public void entrarSistema() {
+		
+	}
+
+	public void sairSistema() {
+		
+	}
+
+	public boolean cadastrarUsuario(String nome, String email, String senha,
+			String cpf, String dataDeNascimento, String sexo, String fone,
+			String identidade, String logradouro, String complemento,
+			String bairro, String cidade, String uf) {
+		return false;
+	}
+
+	public boolean alterarCadastro() {
+		return false;
+	}
+
+	public boolean deletarCadastro() {
+		return false;
+	}
+	
+	public void controleEntrega(){
+		
+	}
+
+}
---
+++ MercadOnline/src/Main/Gerente.java
@@ -0,0 +1,29 @@
+package Main;
+
+public class Gerente extends Usuario {
+
+	public void entrarSistema() {
+		
+	}
+
+	public void sairSistema() {
+		
+	}
+
+	public boolean cadastrarUsuario(String nome, String email, String senha,
+			String cpf, String dataDeNascimento, String sexo, String fone,
+			String identidade, String logradouro, String complemento,
+			String bairro, String cidade, String uf) {
+		return false;
+	}
+
+	public boolean alterarCadastro() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deletarCadastro() {
+		return false;
+	}
+
+}
